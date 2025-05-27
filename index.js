b22e8ca9e7094b94509d582354160dfa594a32cd
abhay-jadhav-767 <abhayjadhav767@gmail.com>
2025-05-27 13:29:43 +0000

    seven

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world hi from abhay" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('123changed not ');
+ res.send('1changed not ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
